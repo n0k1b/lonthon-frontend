b7733f47e42e96b3440b7392fc817b4c2e50feb7
nokib-baybit <nokib@baybit.com>
2023-06-24 19:26:07 +0600

    Login error solved

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -45,7 +45,7 @@ const LoginPage = () => {
       const response = await fetch(`${baseURL}/login`, {
         method: "POST",
         headers: {
-          "Content-Type": "application.json",
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(payload),
       });
